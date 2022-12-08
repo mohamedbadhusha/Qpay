97d0d11f3d0f0adc08f2f6e1843104038533916c
mohamedbadhusha <mohamedbadhusha001@gmail.com>
2022-12-08 11:01:07 +0300

    Update qpay.php

--- upload/admin/controller/extension/payment/qpay.php
+++ upload/admin/controller/extension/payment/qpay.php
@@ -3,12 +3,12 @@ class ControllerPaymentQpay extends Controller {
   private $error = array();
  
   public function index() {
-    $this->language->load('payment/Qpay');
-    $this->document->setTitle('Qpay Payment Method Configuration');
+    $this->language->load('payment/qpay');
+    $this->document->setTitle('qpay Payment Method Configuration');
     $this->load->model('setting/setting');
  
     if (($this->request->server['REQUEST_METHOD'] == 'POST')) {
-      $this->model_setting_setting->editSetting('Qpay', $this->request->post);
+      $this->model_setting_setting->editSetting('qpay', $this->request->post);
       $this->session->data['success'] = 'Saved.';
       $this->redirect($this->url->link('extension/payment', 'token=' . $this->session->data['token'], 'SSL'));
     }
@@ -23,7 +23,7 @@ class ControllerPaymentQpay extends Controller {
     $this->data['text_disabled'] = $this->language->get('text_disabled');
     $this->data['entry_status'] = $this->language->get('entry_status');
  
-    $this->data['action'] = $this->url->link('payment/Qpay', 'token=' . $this->session->data['token'], 'SSL');
+    $this->data['action'] = $this->url->link('payment/qpay', 'token=' . $this->session->data['token'], 'SSL');
     $this->data['cancel'] = $this->url->link('extension/payment', 'token=' . $this->session->data['token'], 'SSL');
  
     if (isset($this->request->post['text_config_one'])) {
@@ -38,21 +38,21 @@ class ControllerPaymentQpay extends Controller {
       $this->data['text_config_two'] = $this->config->get('text_config_two');
     }
             
-    if (isset($this->request->post['Qpay_status'])) {
-      $this->data['Qpay_status'] = $this->request->post['Qpay_status'];
+    if (isset($this->request->post['qpay_status'])) {
+      $this->data['qpay_status'] = $this->request->post['qpay_status'];
     } else {
-      $this->data['Qpay_status'] = $this->config->get('Qpay_status');
+      $this->data['qpay_status'] = $this->config->get('qpay_status');
     }
         
-    if (isset($this->request->post['Qpay_order_status_id'])) {
-      $this->data['Qpay_order_status_id'] = $this->request->post['Qpay_order_status_id'];
+    if (isset($this->request->post['qpay_order_status_id'])) {
+      $this->data['qpay_order_status_id'] = $this->request->post['qpay_order_status_id'];
     } else {
-      $this->data['Qpay_order_status_id'] = $this->config->get('Qpay_order_status_id');
+      $this->data['qpay_order_status_id'] = $this->config->get('qpay_order_status_id');
     }
  
     $this->load->model('localisation/order_status');
     $this->data['order_statuses'] = $this->model_localisation_order_status->getOrderStatuses();
-    $this->template = 'payment/Qpay.tpl';
+    $this->template = 'payment/qpay.tpl';
             
     $this->children = array(
       'common/header',
